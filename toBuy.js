f6599c4478ef151de0756bd0a1a059f3cc1e6979
WanDrewBerry <137406029+WanDrewBerry@users.noreply.github.com>
2023-06-22 19:58:31 +0800

    Add files via upload

---
+++ toBuy.js
@@ -0,0 +1,93 @@
+
+// const inputItem = document.querySelector('#inputItem')
+// const inputQty = document.querySelector('#inputQty')
+// const inputBudget = document.querySelector('#inputBudget')
+
+const form = document.querySelector('form');
+const addButton = document.querySelector('#addButton');
+const listContainer = document.querySelector('#listContainer');
+
+
+// _________ ADD BUTTON
+
+addButton.addEventListener('click', function(e){
+    e.preventDefault()
+    let inputItem = document.querySelector('#inputItem').value;
+    let inputQty = document.querySelector('#inputQty').value;
+    let inputBudget = document.querySelector('#inputBudget').value;
+
+    if(inputItem === '' && inputBudget === ''){
+        alert("You must write an item name and the budget for it.");
+    } 
+    else if(inputItem === ''){
+        alert("You must write an item to add.")
+    }
+    else if(inputBudget === ''){
+        alert("You must provide your budget for the item.")
+    }
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = `${inputQty} ${inputItem} - P ${inputBudget}`;
+        listContainer.appendChild(li);
+
+        // CREATING EDIT ICON
+        let img = document.createElement("img");
+        img.src = "images/edit1.png";
+        li.appendChild(img);
+
+        // CREATING DELETE ICON
+        let deleteSpan = document.createElement('span');
+        deleteSpan.innerHTML = "\u00d7";
+        li.appendChild(deleteSpan);
+
+        form.reset();
+        saveData()
+    }
+})
+
+listContainer.addEventListener('click', function(e){
+
+    // IF THE ITEM IS DONE AND NEED TO CROSS OUT
+    if(e.target.tagName === 'LI'){
+        e.target.classList.toggle("checked");
+        saveData()
+    }
+    // TO REMOVE THE ITEM
+    else if(e.target.tagName === 'SPAN'){
+            e.target.parentElement.remove();
+            saveData()
+    }
+    // TO EDIT THE ITEM
+    else if(e.target.tagName === 'IMG'){
+        
+           let edit = e.target.parentElement.contentEditable = true;
+           edit.focus()
+        }
+    
+       
+    //     let newTextField = e.target.parentElement;
+    //     let newInputField = document.createElement('input');
+    //     newTextField.appendChild(newInputField); 
+    //     e.target.parentElement.value.remove();
+    // }
+            
+})
+
+
+function saveData(){
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function showData(){
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+
+function removeData(){
+    localStorage.removeItem("data");
+}
+function clearData(){
+    localStorage.clearData();
+}
+// clearData()
+// removeData()
+showData()
